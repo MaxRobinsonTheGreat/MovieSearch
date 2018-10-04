9782a92d3d0bfd86f35c5b8ab3a8d1cd3c648423
Max <maxdrob1@gmail.com>
2018-10-04 18:40:58 +0000

    More robust

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
     $( "#submitButton" ).click(function(e) {
     e.preventDefault();
-    var url = "http://www.omdbapi.com/?s="+$("#movie-field").val()+"&apikey=a6a90d52";
+    var url = "http://www.omdbapi.com/?s="+getMovieName()+"&apikey=a6a90d52";
         $.getJSON(url,function(data) {
-            console.log(data)
             var everything;
             everything = "<ul>";
             console.log(data.Search)
             
             for(item of data.Search){
-                everything += "<li> "+ item.Title;
+                everything += "<li> "+ item.Title + "</li>";
             }
             everything += "</ul>";
             $("#movie-list").html(everything);
         }).done(function() {
             console.log('getJSON request succeeded!'); 
-        }).fail(function(jqXHR, textStatus, errorThrown) { 
+        }).fail(function(jqXHR, textStatus, errorThrown) {
             console.log('getJSON request failed! ' + textStatus);
         }).always(function() {
             console.log('getJSON request ended!');
         });
     });
+    
+    function getMovieName(){
+        name = $("#movie-field").val();
+        name.toLowerCase();
+        name.replace(" ", "_");
+        return name;
+    }
 });
