85c6ae0d8f4078d4c461040fd8076d66e942c0f2
jlnichol <jericanicholls@gmail.com>
2018-10-05 05:54:06 +0000

    working function

--- script.js
+++ script.js
@@ -21,36 +21,39 @@ $(document).ready(function() {
             console.log('getJSON request ended!');
         });
     });
-
-    function sortify(data) {
-        rating = data.Metascore;
+var chosenOne = null;
+var chosenPoster = null;
+        var max = 0;
+    function sortify(title, rating, poster) {
         if (rating > max) {
             max = rating;
-            chosenOne = data;
-            console.log(chosenOne);
+            chosenOne = title;
+            chosenPoster = poster;
+            console.log(chosenPoster);
+            
         }
     }
 
-    $("#sorter").click(function(e) {
-        e.preventDefault();
-        var chosenOne = null;
-        var max = 0;
-        for (item of datalist) {
-            var movie = getMovieData(item.Title);
-            console.log("fun "+movie);
-            if(movie.Metascore > max){
-                max = movie.Metascore;
-                chosenOne = movie;
-            }
-        }
-      alert(chosenOne);  
-    });
+$("#reveal").click(function(e) {
+    console.log("chosen: " + chosenOne);
+    var total = "Title: " + chosenOne + "\n";
+    var rater = "Rating: " + max + "\n";
+    var linker = "<img src=\"" + chosenPoster + "\" alt=\"movie\">"
+    $("movie-chosen").html(total);
+    $("#movie-rated").html(rater);
+    $("#movie-posted").html(linker);
+});
     
-    function getMovieData(title){
-        var url = "http://www.omdbapi.com/?t=" + title + "&apikey=a6a90d52";
+    $("#sorter").click(function(e) {
+        max = 0;
+        e.preventDefault();
+        for (item of datalist) {
+        var url = "http://www.omdbapi.com/?t=" + item.Title + "&apikey=a6a90d52";
         $.getJSON(url, function(data) {
-            console.log(data)
-            return data;
+            console.log(data.Metascore)
+            if (data.Metascore != "N/A") {
+            sortify(data.Title, data.Metascore, data.Poster);
+            }
         }).done(function() {
             console.log('getJSON request succeeded!');
         }).fail(function(jqXHR, textStatus, errorThrown) {
@@ -59,6 +62,8 @@ $(document).ready(function() {
             console.log('getJSON request ended!');
         });
     }
+    console.log("HIIIII");
+    });
     
     function getMovieName() {
         name = $("#movie-field").val();
